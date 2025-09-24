b332d04d25debebe79b34bb6ff157ca686bfb7b7
edizon.napa <edizon.napa@proton.me>
2025-09-24 19:43:45 +0800

    Added ingress

--- poc/terraform/aws/eks_2/eks.tf
+++ poc/terraform/aws/eks_2/eks.tf
@@ -43,12 +43,12 @@ resource "aws_eks_cluster" "eks_cluster" {
     }
 }
 
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name  = aws_eks_cluster.eks_cluster.name
-  addon_name    = "aws-ebs-csi-driver"
-  addon_version = "v1.37.0-eksbuild.1" 
-  resolve_conflicts = "OVERWRITE"
-  service_account_role_arn = aws_iam_role.eks_node_role.arn
-  depends_on = [aws_eks_cluster.eks_cluster]
-}
+# resource "aws_eks_addon" "ebs_csi" {
+#   cluster_name  = aws_eks_cluster.eks_cluster.name
+#   addon_name    = "aws-ebs-csi-driver"
+#   addon_version = "v1.37.0-eksbuild.1" 
+#   resolve_conflicts = "OVERWRITE"
+#   service_account_role_arn = aws_iam_role.eks_node_role.arn
+#   depends_on = [aws_eks_cluster.eks_cluster]
+# }
   
--- poc/terraform/aws/eks_2/ingress.tf
+++ poc/terraform/aws/eks_2/ingress.tf
@@ -1,5 +1,5 @@
 
-resource "helm_release" "aws-load-balancer-controller" {
+resource "helm_release" "aws_load_balancer_controller" {
   name             = "aws-load-balancer-controller"
   repository       = "https://aws.github.io/eks-charts"
   chart            = "aws-load-balancer-controller"
@@ -16,55 +16,55 @@ resource "helm_release" "aws-load-balancer-controller" {
         create = true
         name   = "aws-load-balancer-controller"
         annotations = {
-          "eks.amazonaws.com/role-arn" = aws_iam_role.eks_node_role.arn
+          "eks.amazonaws.com/role-arn" = aws_iam_role.aws-load-balancer-controller.arn
         }
       }
-    }),
-    file("./manifest/ingress.yaml")
+    })
+  ]
+
+  timeout    = 900
+  depends_on = [aws_eks_cluster.eks_cluster]
+}
+
+resource "helm_release" "aws_ebs_csi_driver" {
+  name       = "aws-ebs-csi-driver"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart      = "aws-ebs-csi-driver"
+  version    = "2.32.0"
+
+  values = [
+    yamlencode({
+      controller = {
+        serviceAccount = {
+          create = true
+          name   = "ebs-csi-controller-sa"
+          annotations = {
+            "eks.amazonaws.com/role-arn" = aws_iam_role.eks_node_role.arn
+          }
+        }
+        enableVolumeScheduling = true
+        enableVolumeResizing   = true
+        enableVolumeSnapshot   = true
+      }
+      storageClasses = [
+        {
+          name                = "ebs-sc"
+          annotations = {
+            "storageclass.kubernetes.io/is-default-class" = "true"
+          }
+          volumeBindingMode   = "WaitForFirstConsumer"
+          provisioner         = "ebs.csi.aws.com"
+          parameters = {
+            type      = "gp3"
+            encrypted = "true"
+          }
+          allowVolumeExpansion = true
+          reclaimPolicy       = "Delete"
+        }
+      ]
+    })
   ]
 
   depends_on = [aws_eks_cluster.eks_cluster]
 }
-
-# resource "helm_release" "aws_ebs_csi_driver" {
-#   name       = "aws-ebs-csi-driver"
-#   namespace  = "kube-system"
-#   repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
-#   chart      = "aws-ebs-csi-driver"
-#   version    = "2.32.0"
-
-#   values = [
-#     yamlencode({
-#       controller = {
-#         serviceAccount = {
-#           create = true
-#           name   = "ebs-csi-controller-sa"
-#           annotations = {
-#             "eks.amazonaws.com/role-arn" = aws_iam_role.eks_node_role.arn
-#           }
-#         }
-#         enableVolumeScheduling = true
-#         enableVolumeResizing   = true
-#         enableVolumeSnapshot   = true
-#       }
-#       storageClasses = [
-#         {
-#           name                = "ebs-sc"
-#           annotations = {
-#             "storageclass.kubernetes.io/is-default-class" = "true"
-#           }
-#           volumeBindingMode   = "WaitForFirstConsumer"
-#           provisioner         = "ebs.csi.aws.com"
-#           parameters = {
-#             type      = "gp3"
-#             encrypted = "true"
-#           }
-#           allowVolumeExpansion = true
-#           reclaimPolicy       = "Delete"
-#         }
-#       ]
-#     })
-#   ]
-
-#   depends_on = [aws_eks_cluster.eks_cluster]
-# }
